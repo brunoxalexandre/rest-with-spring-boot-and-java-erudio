4ee2e3607f808bc55107c2e37e54c86875a69357
PDigital <brunoxalexandre@gmail.com>
2022-12-04 18:04:59 -0300

    Alterações 13.11.22
    Alterações do dia 13.11.22

---
+++ 05_FirstStepInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Sao Jose', 'Bruno', 'Male', 'Melo'),
+	(2, 'Florianópolis', 'Alexandre', 'Male', 'Nogueira'),
+	(4, 'Rio de Janeiro', 'Tulio', 'Male', 'Maravilha'),
+	(5, 'Rio de Janeiro - RJ', 'Tulio', 'Male', 'Maravilha'),
+	(7, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+	(10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
